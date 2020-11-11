2eba9fa38bda5b0ca6415452c558c38dec4a5681
JSousa <jsousa50@gmail.com>
2020-11-11 17:02:29 +0000

    Initial commit

---
+++ app/src/main/java/com/example/wildips/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.wildips
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.imageView1).setOnClickListener{
+            TODO() // abre a próxima activity
+        }
+    }
+}
